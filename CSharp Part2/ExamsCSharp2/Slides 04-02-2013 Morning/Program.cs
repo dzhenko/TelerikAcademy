a1db86e913897302605270d8f15600e5256616dc
fast4y <dzhenko@gmail.com>
2014-01-08 20:44:56 +0200

    s

--- CSharp Part2/ExamsCSharp2/Slides 04-02-2013 Morning/Program.cs
+++ CSharp Part2/ExamsCSharp2/Slides 04-02-2013 Morning/Program.cs
@@ -5,21 +5,232 @@ using System.Text;
 
 class Program
 {
-    public static int[] WHD;
 
     public static string[, ,] cube;
+
+    public static int[] ball;
 
 
 
     static void Main(string[] args)
     {
-        WHD = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        CreateCube();
+
+        GetBallInitialPosition();
+
+        MoveTheBall();
+    }
+
+    private static void MoveTheBall()
+    {
+        while (true)
+        {
+            //Fall trough - just increaze hight
+            if (cube[ball[0], ball[1], ball[2]] == "E")
+            {
+                MoveTroughHoleE();
+            }
+
+            //teleport
+            else if (cube[ball[0], ball[1], ball[2]][0] == 'T')
+            {
+                TeleportTheBall();
+            }
+            else if (cube[ball[0], ball[1], ball[2]] == "B")
+            {
+                GameOver(ball);
+            }
+            else
+            {
+                //we are sliding
+
+                //if we are sliding on the last row (height) => we win
+                if (ball[1] == cube.GetLength(1) - 1)
+                {
+                    LastHeightSlideWin();
+                }
+
+                else if (cube[ball[0], ball[1], ball[2]] == "S F")
+                {
+                    MoveForward();
+                }
+                else if (cube[ball[0], ball[1], ball[2]] == "S B")
+                {
+                    MoveBackwards();
+                }
+                else if (cube[ball[0], ball[1], ball[2]] == "S R")
+                {
+                    MoveRight();
+                }
+                else if (cube[ball[0], ball[1], ball[2]] == "S L")
+                {
+                    MoveLeft();
+                }
+                //W H D
+                else if (cube[ball[0], ball[1], ball[2]] == "S FR")
+                {
+                    MoveForwardRight();
+                }
+                else if (cube[ball[0], ball[1], ball[2]] == "S BR")
+                {
+                    MoveBackwardsRight();
+                }
+                else if (cube[ball[0], ball[1], ball[2]] == "S FL")
+                {
+                    MoveForwardLeft();
+                }
+                else if (cube[ball[0], ball[1], ball[2]] == "S BL")
+                {
+                    MoveBackwardsLeft();
+                }
+
+                //the ball always increazes it's height
+                ball[1]++;
+            }
+        }
+    }
+
+    private static void LastHeightSlideWin()
+    {
+        Console.WriteLine("Yes");
+        Console.WriteLine("{0} {1} {2}", ball[0], ball[1], ball[2]);
+        Environment.Exit(0);
+    }
+
+    private static void MoveTroughHoleE()
+    {
+        ball[1]++;
+
+        //check if we are out of the cube (if this was a hole at the last row of the cube)
+        if (ball[1] >= cube.GetLength(1))
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine("{0} {1} {2}", ball[0], ball[1] - 1, ball[2]);
+            Environment.Exit(0);
+        }
+    }
+
+    private static void MoveBackwardsLeft()
+    {
+        if (ball[2] == cube.GetLength(2) - 1)
+        {
+            GameOver(ball);
+        }
+        if (ball[0] == 0)
+        {
+            GameOver(ball);
+        }
+        ball[0]--;
+        ball[2]++;
+    }
+
+    private static void MoveForwardLeft()
+    {
+        if (ball[2] == 0)
+        {
+            GameOver(ball);
+        }
+        if (ball[0] == 0)
+        {
+            GameOver(ball);
+        }
+        ball[0]--;
+        ball[2]--;
+    }
+
+    private static void MoveBackwardsRight()
+    {
+        if (ball[2] == cube.GetLength(2) - 1)
+        {
+            GameOver(ball);
+        }
+        if (ball[0] == cube.GetLength(0) - 1)
+        {
+            GameOver(ball);
+        }
+        ball[0]++;
+        ball[2]++;
+    }
+
+    private static void MoveForwardRight()
+    {
+        if (ball[2] == 0)
+        {
+            GameOver(ball);
+        }
+        if (ball[0] == cube.GetLength(0) - 1)
+        {
+            GameOver(ball);
+        }
+        ball[0]++;
+        ball[2]--;
+    }
+
+    private static void MoveLeft()
+    {
+        if (ball[0] == 0)
+        {
+            GameOver(ball);
+        }
+        ball[0]--;
+    }
+
+    private static void MoveRight()
+    {
+        if (ball[0] == cube.GetLength(0) - 1)
+        {
+            GameOver(ball);
+        }
+        ball[0]++;
+    }
+
+    private static void MoveBackwards()
+    {
+        if (ball[2] == cube.GetLength(2) - 1)
+        {
+            GameOver(ball);
+        }
+        ball[2]++;
+    }
+
+    private static void MoveForward()
+    {
+        if (ball[2] == 0)
+        {
+            GameOver(ball);
+        }
+        ball[2]--;
+    }
+
+    private static void TeleportTheBall()
+    {
+        int[] teleportCoords = cube[ball[0], ball[1], ball[2]].TrimStart('T').Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        ball[0] = teleportCoords[0];
+        ball[2] = teleportCoords[1];
+    }
+
+    private static void GetBallInitialPosition()
+    {
+        int[] ballStartCoords = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        ball = new int[] { ballStartCoords[0], 0, ballStartCoords[1] };
+    }
+
+    private static void CreateCube()
+    {
+        int[] WHD = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
         cube = new string[WHD[0], WHD[1], WHD[2]];
 
+        ReadCubeFromConsole(WHD);
+    }
+
+    private static void ReadCubeFromConsole(int[] WHD)
+    {
         for (int h = 0; h < WHD[1]; h++)
         {
-            string[] currLine = Console.ReadLine().Split(new char[] { '|'},StringSplitOptions.RemoveEmptyEntries);
+            string[] currLine = Console.ReadLine().Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
             for (int d = 0; d < WHD[2]; d++)
             {
                 string[] currItems = currLine[d].Split(new char[] { ')' }, StringSplitOptions.RemoveEmptyEntries);
@@ -27,131 +238,6 @@ class Program
                 {
                     cube[w, h, d] = currItems[w].TrimStart().TrimStart('(');
                 }
-            }
-        }
-
-        int[] ballStartCoords = Console.ReadLine().Split().Select(int.Parse).ToArray();
-
-        int[] ball = new int[] { ballStartCoords[0], 0, ballStartCoords[1] };
-
-        while (true)
-        {
-            if (cube[ball[0],ball[1],ball[2]] == "E")
-            {
-                ball[1]++;
-            }
-            else if (cube[ball[0],ball[1],ball[2]][0] == 'T')
-            {
-                int[] teleportCoords = cube[ball[0], ball[1], ball[2]].TrimStart('T').Split(new char[] { ' ' },
-                    StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                ball[0] = teleportCoords[0];
-                ball[2] = teleportCoords[1];
-            }
-            else if (cube[ball[0],ball[1],ball[2]] == "B")
-            {
-                GameOver(ball);
-            }
-            else
-            {
-                if (ball[1] == cube.GetLength(1) - 1)
-                {
-                    Console.WriteLine("Yes");
-                    Console.WriteLine("{0} {1} {2}", ball[0], ball[1], ball[2]);
-                    return;
-                }
-                else if (cube[ball[0],ball[1],ball[2]] == "S F")
-                {
-                    if (ball[2] == 0)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[2]--;
-                }
-                else if (cube[ball[0],ball[1],ball[2]] == "S B")
-                {
-                    if (ball[2] == cube.GetLength(2) - 1)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[2]++;
-                }
-                else if (cube[ball[0],ball[1],ball[2]] == "S R")
-                {
-                    if (ball[0] == cube.GetLength(0) - 1)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[0]++;
-                }
-                else if (cube[ball[0], ball[1], ball[2]] == "S L")
-                {
-                    if (ball[0] == 0)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[0]--;
-                }
-                //W H D
-                else if (cube[ball[0], ball[1], ball[2]] == "S FR")
-                {
-                    if (ball[2] == 0)
-                    {
-                        GameOver(ball);
-                    }
-                    if (ball[0] == cube.GetLength(0) - 1)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[0]++;
-                    ball[2]--;
-                }
-                else if (cube[ball[0], ball[1], ball[2]] == "S BR")
-                {
-                    if (ball[2] == cube.GetLength(2) - 1)
-                    {
-                        GameOver(ball);
-                    }
-                    if (ball[0] == cube.GetLength(0) - 1)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[0]++;
-                    ball[2]++;
-                }
-                else if (cube[ball[0], ball[1], ball[2]] == "S FL")
-                {
-                    if (ball[2] == 0)
-                    {
-                        GameOver(ball);
-                    }
-                    if (ball[0] == 0)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[0]--;
-                    ball[2]--;
-                }
-                else if (cube[ball[0], ball[1], ball[2]] == "S BL")
-                {
-                    if (ball[2] == cube.GetLength(2) - 1)
-                    {
-                        GameOver(ball);
-                    }
-                    if (ball[0] == 0)
-                    {
-                        GameOver(ball);
-                    }
-                    ball[0]--;
-                    ball[2]++;
-                }
-                ball[1]++;
-            }
-
-            if (ball[1] >= cube.GetLength(1))
-            {
-                Console.WriteLine("Yes");
-                Console.WriteLine("{0} {1} {2}", ball[0], ball[1] - 1, ball[2]);
-                break;
             }
         }
     }
