fc91433a446600c2c9e5cc1a4b5a898ac2c4621d
fast4y <dzhenko@gmail.com>
2014-01-13 18:17:48 +0200

    c

--- CSharp Part2/ExamsCSharp2/Laser 05-02-2013/Program.cs
+++ CSharp Part2/ExamsCSharp2/Laser 05-02-2013/Program.cs
@@ -35,30 +35,75 @@ class Program
                 }
             }
 
-            if (burned[laser[0] + direction[0],laser[1] + direction[1],laser[2] + direction[2]])
-            {
-                Console.WriteLine("{0} {1} {2}",laser[0] + 1,laser[1] + 1,laser[2] + 1);
-                return;
-            }
-
             laser[0] += direction[0];
             laser[1] += direction[1];
             laser[2] += direction[2];
+
+            if (burned[laser[0],laser[1],laser[2]])
+            {
+                Console.WriteLine("{0} {1} {2}", laser[0] - direction[0] + 1, laser[1] - direction[1] + 1, laser[2] - direction[2] + 1);
+                return;
+            }
+            else
+            {
+                burned[laser[0], laser[1], laser[2]] = true;
+            }
 
             if (AreWeOnEdge())
             {
                 Console.WriteLine("{0} {1} {2}",laser[0] + 1 - direction[0],laser[1] + 1- direction[1],laser[2] + 1- direction[2]);
                 return;
             }
+            //for (int i = 0; i < burned.GetLength(1); i++)
+            //{
+            //    for (int j = 0; j < burned.GetLength(2); j++)
+            //    {
+            //        if (i == laser[1] && j == laser[2])
+            //        {
+            //            Console.Write('X');
+            //        }
+            //        else
+            //        {
+            //            Console.Write('*');
+            //        }
+            //    }
+            //    Console.Write(i);
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine(dimensions[1] + " " + dimensions[1]);
+            //Console.WriteLine(laser[0] + " " + laser[2]);
+            //Console.ReadLine();
+            //Console.Clear();
         }
     }
     static bool AreWeOnEdge()
     {
-        if ((laser[0] == 0 || laser[0] == burned.GetLength(0) - 1) &&
-        (laser[1] == 0 || laser[1] == burned.GetLength(1) - 1) &&
-        (laser[2] == 0 || laser[2] == burned.GetLength(2) - 1))
+        //if ((laser[0] == 0 || laser[0] == burned.GetLength(0) - 1) &&
+        //(laser[1] == 0 || laser[1] == burned.GetLength(1) - 1) &&
+        //(laser[2] == 0 || laser[2] == burned.GetLength(2) - 1))
+        //{
+        //    return true;
+        //}
+        if (laser[1] == 0) // botom side
         {
-            return true;
+            if (laser[0] == 0 || laser[0] == burned.GetLength(0) - 1 || laser[2] == 0 || laser[2] == burned.GetLength(2) - 1)
+            {
+                return true;
+            }
+        }
+        else if (laser[1] == burned.GetLength(1) - 1) // top side
+        {
+            if (laser[0] == 0 || laser[0] == burned.GetLength(0) - 1 || laser[2] == 0 || laser[2] == burned.GetLength(2) - 1)
+            {
+                return true;
+            }
+        }
+        else // columns
+        {
+            if ((laser[0] == 0 || laser[0] == burned.GetLength(0) - 1) && (laser[2] == 0 || laser[2] == burned.GetLength(2) - 1))
+            {
+                return true;
+            }
         }
         return false;
     }
