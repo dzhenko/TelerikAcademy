69d5632afa2df2c45abcdc2a054d71385d3c60d4
fast4y <dzhenko@gmail.com>
2014-03-15 09:04:21 +0200

    a

--- DSA/DSA-8-Recursion-Old/12. QueensProblem/QueensProblem.cs
+++ DSA/DSA-8-Recursion-Old/12. QueensProblem/QueensProblem.cs
@@ -1,5 +1,6 @@
 ﻿//* Write a recursive program to solve the "8 Queens Puzzle" with backtracking. Learn more at: http://en.wikipedia.org/wiki/Eight_queens_puzzle
 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
