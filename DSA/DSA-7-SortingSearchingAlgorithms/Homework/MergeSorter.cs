bbd462446ed33457087b759dc71e5eb6b0e4d179
fast4y <dzhenko@gmail.com>
2014-03-15 22:58:58 +0200

    asd

---
+++ DSA/DSA-7-SortingSearchingAlgorithms/Homework/MergeSorter.cs
@@ -0,0 +1,81 @@
+﻿namespace SortingHomework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MergeSorter<T> : ISorter<T> where T : IComparable<T>
+    {
+        public void Sort(IList<T> collection)
+        {
+            var sorted = this.MergeSort(collection);
+
+            collection.Clear();
+
+            foreach (var sortedItem in sorted)
+            {
+                collection.Add(sortedItem);
+            }
+        }
+
+        private List<T> MergeSort(IList<T> collection)
+        {
+            if (collection.Count < 2)
+            {
+                return collection.ToList();
+            }
+
+            List<T> left = new List<T>();
+            List<T> right = new List<T>();
+
+            for (int i = 0; i < collection.Count/2; i++)
+            {
+                left.Add(collection[i]);
+            }
+
+            for (int i = collection.Count / 2; i < collection.Count; i++)
+            {
+                right.Add(collection[i]);
+            }
+
+            left = this.MergeSort(left);
+            right = this.MergeSort(right);
+
+            return this.MergeThoseDamnLists(left, right);
+        }
+
+        private List<T> MergeThoseDamnLists(IList<T> left, IList<T> right)
+        {
+            List<T> answer = new List<T>(left.Count + right.Count);
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            for (int i = 0; i < answer.Capacity; i++)
+            {
+                if (leftIndex >= left.Count)
+                {
+                    answer.Add(right[rightIndex]);
+                    rightIndex++;
+                }
+                else if (rightIndex >= right.Count)
+                {
+                    answer.Add(left[leftIndex]);
+                    leftIndex++;
+                }
+                else if (left[leftIndex].CompareTo(right[rightIndex]) > 0)
+                {
+                    answer.Add(right[rightIndex]);
+                    rightIndex++;
+                }
+                else
+                {
+                    answer.Add(left[leftIndex]);
+                    leftIndex++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ DSA/DSA-7-SortingSearchingAlgorithms/Homework/UnitTesting.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingHomework
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        [TestMethod]
+        public void SelectionSort()
+        {
+            var collection = new SortableCollection<int>(new[] { 5, 1, 2, 4, 0, 6, 9, 8, 7 });
+
+            collection.Sort(new SelectionSorter<int>());
+
+            for (int i = 0; i < collection.Items.Count - 1; i++)
+            {
+                Assert.IsTrue(collection.Items[i] < collection.Items[i + 1]);
+            }
+        }
+
+        [TestMethod]
+        public void QuickSort()
+        {
+            var collection = new SortableCollection<int>(new[] { 5, 1, 2, 4, 0, 6, 9, 8, 7 });
+
+            collection.Sort(new Quicksorter<int>());
+
+            for (int i = 0; i < collection.Items.Count - 1; i++)
+            {
+                Assert.IsTrue(collection.Items[i] < collection.Items[i + 1]);
+            }
+        }
+
+        [TestMethod]
+        public void MergeSort()
+        {
+            var collection = new SortableCollection<int>(new[] { 5, 1, 2, 4, 0, 6, 9, 8, 7 });
+
+            collection.Sort(new MergeSorter<int>());
+
+            for (int i = 0; i < collection.Items.Count - 1; i++)
+            {
+                Assert.IsTrue(collection.Items[i] < collection.Items[i + 1]);
+            }
+        }
+
+        [TestMethod]
+        public void LinearSearchLast()
+        {
+            var collection = new SortableCollection<int>(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
+
+            bool result = collection.LinearSearch(0);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void BinarySearch()
+        {
+            var collection = new SortableCollection<int>(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+            bool result = collection.BinarySearch(8);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Shuffle()
+        {
+            var collection = new SortableCollection<int>(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+            collection.Shuffle();
+            bool shuffled = false;
+
+            for (int i = 0; i < collection.Items.Count - 1; i++)
+            {
+                if (collection.Items[i] > collection.Items[i + 1])
+                {
+                    shuffled = true;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(shuffled);
+        }
+    }
+}
