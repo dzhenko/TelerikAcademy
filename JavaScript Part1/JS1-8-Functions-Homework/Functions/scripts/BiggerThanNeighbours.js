5a265f714991b3f5f74eb6db297388f3c417fbc2
fast4y <dzhenko@gmail.com>
2014-04-20 12:31:43 +0300

    a

---
+++ JavaScript Part1/JS1-8-Functions-Homework/Functions/scripts/BiggerThanNeighbours.js
@@ -0,0 +1,16 @@
+﻿function BiggerThanNeighbours(arr,position) {
+    if (position >= arr.length) {
+        return false;
+    }
+
+    if (arr[position] == 0 || arr[position] == arr.length - 1) {
+        return false;
+    }
+
+    if (arr[position] > arr[position-1] && arr[position] > arr[position+1]) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
