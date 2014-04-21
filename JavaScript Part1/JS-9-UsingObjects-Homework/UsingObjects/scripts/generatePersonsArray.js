46eade4e7c4e8d35e0a08b0d0222cdbf1792e2ce
fast4y <dzhenko@gmail.com>
2014-04-21 20:24:34 +0300

    a

---
+++ JavaScript Part1/JS-9-UsingObjects-Homework/UsingObjects/scripts/generatePersonsArray.js
@@ -0,0 +1,28 @@
+﻿function generatePersonsArray(count) {
+
+    var count = count || 20;
+
+    var firstNames = ["Ivan", "Dragan", "Petko", "Gosho", "Pesho"];
+    var lastNames = ["Ivanov", "Draganov", "Petkov", "Dimitrov" , "Aprilov"];
+    var ages = [18, 22, 27, 42, 65];
+
+    function getPerson(fname, lname, age) {
+        return {
+            firstName: fname,
+            lastName: lname,
+            age: age,
+        };
+    }
+
+    var persons = new Array(count);
+
+    for (var i = 0; i < count; i++) {
+        persons[i] = getPerson(
+                firstNames[Math.floor((Math.random() * firstNames.length))],
+                lastNames[Math.floor((Math.random() * lastNames.length))],
+                ages[Math.floor((Math.random() * ages.length))]
+            );
+    }
+
+    return persons;
+}
