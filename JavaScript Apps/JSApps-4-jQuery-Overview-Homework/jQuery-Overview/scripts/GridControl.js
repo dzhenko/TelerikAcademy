50b0dda564e83fcd6718a3a6fa0ed65a7a3a48ac
fast4y <dzhenko@gmail.com>
2014-05-27 10:44:59 +0300

    prr

---
+++ JavaScript Apps/JSApps-4-jQuery-Overview-Homework/jQuery-Overview/scripts/GridControl.js
@@ -0,0 +1,51 @@
+﻿function getGridControl(element) {
+    var currTable = document.createElement('table');
+
+    if (element instanceof HTMLTableRowElement) {
+
+    }
+    else {
+        element.appendChild(currTable);
+    }
+    
+    return {
+        addRow: function () {
+            var newRow = document.createElement('tr');
+
+            var currTBody = currTable.getElementsByTagName('tbody');
+            if (!currTBody) {
+                currTBody = document.createElement('tbody');
+                currTable.appendChild(currTBody);
+            }
+
+            currTBody.appendChild(newRow);
+
+            for (var i = 0; i < arguments.length; i++) {
+                var currCell = document.createElement('td');
+                currCell.innerText = arguments[i];
+                newRow.appendChild(currCell);
+            }
+
+            return getGridControl(newRow);
+        },
+
+        addHead: function () {
+            var oldHead = currTable.getElementsByTagName('thead');
+            if (oldHead) {
+                currTable.removeChild(oldHead);
+            }
+
+            var thead = document.createElement('thead');
+            currTable.appendChild(thead);
+
+            var theadRow = document.createElement('tr');
+            thead.appendChild(theadRow);
+
+            for (var i = 0; i < arguments.length; i++) {
+                var currHead = document.createElement('th');
+                currHead.innerText = arguments[i];
+                theadRow.appendChild(currHead);
+            }
+        }
+    }
+}
---
+++ JavaScript Apps/JSApps-4-jQuery-Overview-Homework/jQuery-Overview/scripts/StudentsArray.js
@@ -0,0 +1,8 @@
+﻿function getAllStudents() {
+    return [
+        { firstName: 'Peter', lastName: 'Ivanov', grade: 3 },
+        { firstName: 'Milena', lastName: 'Grigorova', grade: 6 },
+        { firstName: 'Gergana', lastName: 'Borisova', grade: 12 },
+        { firstName: 'Boyko', lastName: 'Petrov', grade: 7 }
+    ];
+}
