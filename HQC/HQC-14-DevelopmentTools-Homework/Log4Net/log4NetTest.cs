379feb96b75c5c750bff0bd98bd050eda3e9a1a5
fast4y <dzhenko@gmail.com>
2014-06-29 22:42:50 +0300

    prr

---
+++ HQC/HQC-14-DevelopmentTools-Homework/Log4Net/log4NetTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Log4Net
+{
+    using log4net;
+    using log4net.Config;
+    public class Log4NetTest
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Log4NetTest));
+        public static void Main()
+        {
+            BasicConfigurator.Configure();
+
+            logger.Info("Info log");
+            logger.Error("Error log");
+            logger.Warn("Warn log");
+            logger.Fatal("Fatal log");
+        }
+    }
+}
