4fc36b6b585fb2dc77a9d4c10e3124168a6619f2
fast4y <dzhenko@gmail.com>
2014-08-25 21:40:58 +0300

    prr

---
+++ DSA/DSA-2-LinearDataStructures/06. RemoveOddNumberOfTimes/RemoveOddNumberOfTimes.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+/// <summary>
+/// Тествам 3 начина - 2 с речник и един с HashSet - оказва се (за ~11 000 000 елемента, че варианта с речник си е най-бърз когато отговора остане в IEnumerable). Когато се обърне в лист отговора, начина с HashSet се оказва малко по-бърз.
+/// </summary>
+// Write a program that removes from given sequence all numbers that occur odd number of times. Example:
+// {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2}  {5, 3, 3, 5}
+
+public class RemoveOddNumberOfTimes
+{
+    public static void Main()
+    {
+        var input = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+        for (int i = 0; i < 20; i++)
+        {
+            input.AddRange(input);
+        }
+
+        Console.WriteLine("Testing with {0} elements:", input.Count);
+
+        HashSetWay(input);
+
+        FirstDictWay(input);
+
+        SecondDictWay(input);
+    }
+
+    public static void HashSetWay(IList<int> input)
+    {
+        Console.WriteLine("Hash set");
+        var sw = new Stopwatch();
+        sw.Start();
+        var table = new HashSet<int>();
+
+        foreach (var num in input)
+        {
+            if (table.Contains(num))
+            {
+                table.Remove(num);
+            }
+            else
+            {
+                table.Add(num);
+            }
+        }
+
+        Console.WriteLine(sw.ElapsedTicks + " hash set formed");
+
+        var answer = input.Where(x => table.Contains(x));
+        Console.WriteLine(sw.ElapsedTicks + " answer formed");
+
+        answer = answer.ToList();
+        Console.WriteLine(sw.ElapsedTicks + " answer to list");
+    }
+
+    public static void FirstDictWay(IList<int> input)
+    {
+        Console.WriteLine("Standard dict way");
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            if (!dict.ContainsKey(input[i]))
+            {
+                dict.Add(input[i], 1);
+            }
+            else
+            {
+                dict[input[i]]++;
+            }
+        }
+        Console.WriteLine(sw.ElapsedTicks + " dict formed");
+
+        var answer = input.Where(x => dict[x] % 2 == 0);
+        Console.WriteLine(sw.ElapsedTicks + " answer formed");
+
+        answer = answer.ToList();
+        Console.WriteLine(sw.ElapsedTicks + " answer to list");
+    }
+
+    public static void SecondDictWay(IList<int> input)
+    {
+        Console.WriteLine("Dict with lambda");
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var dict = input.GroupBy(x => x).ToDictionary(gr => gr.Key, gr => gr.Count());
+        Console.WriteLine(sw.ElapsedTicks + " dict formed");
+
+        var answer = input.Where(x => dict[x] % 2 == 0);
+        Console.WriteLine(sw.ElapsedTicks + " answer formed");
+
+        answer = answer.ToList();
+        Console.WriteLine(sw.ElapsedTicks + " answer to list");
+    }
+}
---
+++ DSA/DSA-2-LinearDataStructures/14. Maze/Maze.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Maze
+{
+    class Program
+    {
+        //marking x as -1 and start as -2
+        public static int[,] maze = new int[,] {{0,0,0,-1,0,-1},
+                                                {0,-1,0,-1,0,-1},
+                                                {0,-2,-1,0,-1,0} ,
+                                                {0,-1,0,0,0,0},
+                                                {0,0,0,-1,-1,0},
+                                                {0,0,0,-1,0,-1}};
+
+        static void Main(string[] args)
+        {
+            var startRow = 0;
+            var startCol = 0;
+
+            FindStart(ref startRow, ref startCol);
+
+            Solve(startRow, startCol, 0);
+
+            PrintAswer();
+        }
+
+        static void Solve(int row, int col, int step)
+        {
+            if (row < 0 || col < 0 || row >= maze.GetLength(0) || col >= maze.GetLength(1) || maze[row, col] == -1)
+            {
+                return;
+            }
+
+            if (maze[row, col] < step && maze[row, col] > 0)
+            {
+                return;
+            }
+
+            if (maze[row, col] == 0 || maze[row, col] > step)
+            {
+                maze[row, col] = step;
+            }
+
+            Solve(row + 1, col, step + 1);
+            Solve(row - 1, col, step + 1);
+            Solve(row, col + 1, step + 1);
+            Solve(row, col - 1, step + 1);
+        }
+
+        static void FindStart(ref int startRow, ref int startCol)
+        {
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    if (maze[i, j] == -2)
+                    {
+                        startRow = i;
+                        startCol = j;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void PrintAswer()
+        {
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    if (maze[i, j] == -2)
+                    {
+                        Console.Write(" *");
+                    }
+                    else if (maze[i, j] == -1)
+                    {
+                        Console.Write(" x");
+                    }
+                    else if (maze[i, j] == 0)
+                    {
+                        Console.Write(" u");
+                    }
+                    else
+                    {
+                        Console.Write(" " + maze[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
